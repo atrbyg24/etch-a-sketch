af0be34803c48cf4de07dd9fed089afbfcf9f712
atrbyg24 <artwan520@gmail.com>
2025-01-08 17:06:55 -0500

    Write js function to create 16 x 16 grid for sketchpad

---
+++ script.js
@@ -0,0 +1,21 @@
+let gridSize = 16;
+
+function makeGrid(gridSize) {
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.setAttribute("style",
+        "display: flex; justify-content: space-evenly; flex-wrap: wrap; padding: 0; margin: 0;");
+    for (let i = 0;i < gridSize * gridSize;i++) {
+        const gridBox = document.createElement("div");
+        gridBox.setAttribute("class","grid-box");
+        gridBox.setAttribute("style",
+            "background-color: white; border: 1px solid black; padding: 0; margin: 0; box-sizing: border-box;");
+        let boxSize = 480/gridSize;
+        boxSize = boxSize + "px";
+        gridBox.style.height = boxSize;
+        gridBox.style.width = boxSize;
+        gridContainer.appendChild(gridBox);
+    }
+}
+
+makeGrid(gridSize);
+
